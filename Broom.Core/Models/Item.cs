7f945e5d798134bff3ea81ca310a1aa774afa694
Starinin Andrey <starininandrey@gmail.com>
2024-03-15 16:50:04 +0300

    Добавление класса для описания объекта, с которым мы будем работать

---
+++ Broom.Core/Models/Item.cs
@@ -0,0 +1,7 @@
+﻿namespace Broom.Core.Models;
+
+public record Item
+{
+    public string Name { get; set; }
+    public Uri Path { get; set; }
+}
---
+++ Broom.Core/Models/Items.cs
@@ -0,0 +1,17 @@
+﻿namespace Broom.Core.Models;
+
+using System.Text.Json;
+
+public class Items
+{
+    private readonly List<Item> _items;
+
+    public Items(List<Item> items) => this._items = items;
+
+    public Items()
+    {
+        _items = new List<Item>();
+    }
+
+    public static Items FromJson(string json) => JsonSerializer.Deserialize<Items>(json); //TODO !!!
+}
